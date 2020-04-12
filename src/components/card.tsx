d11b11799d0e154472282a2356d45b5f3deba705
Joshua-Rose <joshua.rose1@outlook.com>
2020-04-11 22:52:21 -0500

    update content of cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import www from '../assets/www.svg';
+import Github from '../assets/Github.svg';
 
 interface Props {
   logo: string | JSX.Element,
@@ -9,9 +11,13 @@ interface Props {
 
 }
 
-const card: React.FC<Props> = ({ logo, headline, body }) => (
+const card: React.FC<Props> = ({
+  logo, headline, body, siteLink, sourceLink,
+}: Props) => (
   <div className="card">
-    {typeof logo === 'string' ? <img src={logo} alt={headline} style={{ maxWidth: '45%' }} /> : logo}
+    <a href={siteLink} className="site" title="View the site"><img src={www} alt="globe" /></a>
+    <a href={sourceLink} className="source" title="View the source"><img src={Github} alt="github" /></a>
+    {typeof logo === 'string' ? <img src={logo} alt={headline} className="primaryLogo" /> : logo}
     <h2>{headline}</h2>
     {body}
   </div>
